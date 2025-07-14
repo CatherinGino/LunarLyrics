3b57b5621586dee6c0732f59f42f13ae30646e32
CatherinGino <catheringino@gmail.com>
2025-07-14 15:53:42 +0530

    changes made in port

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
